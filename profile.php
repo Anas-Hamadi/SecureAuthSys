cbc50104c59124a4c3f806f663059dbfc5e2179a
Anas-Hamadi <nshamadi@gmail.com>
2022-05-25 14:08:37 +0100

    Update

--- profile.php
+++ profile.php
@@ -8,46 +8,46 @@
 	<meta charset="UTF-8">
 	<meta name="csrf_token" content="<?php echo createToken(); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Register - Secure Site</title>
+	<title>Update - Secure Site</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/style.css' ?>" />
 </head>
 <body>
 	
 	<div class="formWrapper">
-		<form id="registerForm">
-			<h1>Register</h1>
+		<form id="updateForm">
+			<h1>Update Profile <?php echo $_SESSION['name'] ?></h1>
 			<div id="errs" class="errcontainer"></div>
 			<div class="inputblock">
 				<label for="name">Name</label>
-				<input id="name" name="name" type="text" autocomplete="name" placeholder="Enter your name" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+				<input id="name" name="name" type="text" autocomplete="name" value="<?php echo $_SESSION['name']?>" onkeydown="if(event.key === 'Enter'){event.preventDefault();update();}" />
 			</div>
 			<div class="inputblock">
 				<label for="email">Email</label>
-				<input id="email" name="email" type="email" autocomplete="email" placeholder="Enter your email" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+				<input id="email" name="email" type="email" autocomplete="email" value="<?php echo $_SESSION['email']?>" onkeydown="if(event.key === 'Enter'){event.preventDefault();update();}" />
 			</div>
 			<div class="inputblock">
 				<label for="password">Password</label>
-				<input id="password" name="password" type="password" autocomplete="new-password" placeholder="Enter your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+				<input id="password" name="password" type="password" autocomplete="new-password" placeholder="password" onkeydown="if(event.key === 'Enter'){event.preventDefault();update();}" />
 			</div>
 			<div class="inputblock">
 				<label for="confirm-password">Confirm Password</label>
-				<input id="confirm-password" name="confirm-password" type="password" autocomplete="new-password" placeholder="Confirm your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+				<input id="confirm-password" name="confirm-password" type="password" autocomplete="new-password" placeholder="confirm password" onkeydown="if(event.key === 'Enter'){event.preventDefault();update();}" />
 			</div>
 			<br>
-			<div class="btn" onclick="register();">Sign Up</div>
+			<div class="btn" onclick="update();">Sign Up</div>
 			<br>
 			<br>
 			<br>
-			<a href="./login">Already have an account? Log In</a>
+			<a href="./">Get back to home? Back</a>
 		</form>
 		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
 		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
 	</div>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="<?php echo dirname($_SERVER['PHP_SELF']) . '/script.js' ?>"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -207,6 +207,102 @@ function register() {
     }, 10);
   });
 }
+// update.php
+function update() {
+  request("php/update.php", "#updateForm", function (data) {
+    document.getElementById("errs").innerHTML = "";
+    var transition = document.getElementById("errs").style.transition;
+    document.getElementById("errs").style.transition = "none";
+    document.getElementById("errs").style.opacity = 0;
+    try {
+      data = JSON.parse(data);
+      if (!(data instanceof Array)) {
+        throw Exception("bad data");
+      }
+
+      //Show errors to user
+      for (var i = 0; i < data.length; ++i) {
+        switch (data[i]) {
+          case 0:
+            document.getElementById("errs").innerHTML +=
+              "<div>Your account has been updated!</div>";
+            document.getElementById("registerForm").reset();
+            break;
+          case 1:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Invalid name entered. (only use letters, spaces, and hyphens)</div>';
+            break;
+          case 2:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Invalid email entered.</div>';
+            break;
+          case 3:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Email does not exist. (This domain does not have a mail server)</div>';
+            break;
+          case 4:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Password must contain: <ul><li>At least 8 characters</li><li>At least one lower case letter</li><li>At least one upper case letter</li><li>At least one number</li><li>At least one special character (~?!@#$%^&*)</li></ul></div>';
+            break;
+          case 5:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Passwords do not match. Please re-enter your confirmed password.</div>';
+            break;
+          case 6:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Failed to add account to database. Please try again later.</div>';
+            break;
+          case 7:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">An account with this email already exists</div>';
+            break;
+          case 8:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Failed to connect to the database. Please try again later.</div>';
+            break;
+          case 9:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Invalid CSRF Token. Please try again later.</div>';
+            break;
+          case 10:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Failed to send email. Please try again later.</div>';
+            break;
+          case 11:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Failed to insert request into database. Please try again later.</div>';
+            break;
+          case 12:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">You have exceeded your number of allowed validation requests per day</div>';
+            break;
+          case 13:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">The user with this email is already validated</div>';
+            break;
+          case 14:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">A user with this email does not exist</div>';
+            break;
+          case 15:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">Failed to connect to database. Please try again later.</div>';
+            break;
+          default:
+            document.getElementById("errs").innerHTML +=
+              '<div class="err">An unknown error occurred. Please try again later. (in the default)</div>';
+        }
+      }
+    } catch (e) {
+      document.getElementById("errs").innerHTML =
+        '<div class="err">An unknown error occurred. Please try again later. (in the catch)</div>';
+    }
+    setTimeout(function () {
+      document.getElementById("errs").style.transition = transition;
+      document.getElementById("errs").style.opacity = 1;
+    }, 10);
+  });
+}
 
 // validateEmail.php
 function sendValidateEmailRequest() {
